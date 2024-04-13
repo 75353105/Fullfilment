330a230768bd3d77bb48e6fabe85d8b00862b562
Rares2442 <raresinator66@gmail.com>
2024-04-13 20:38:41 +0300

    TextView avertizare Sign Up

--- app/src/main/java/com/example/fullfilment_v3/signup/SignUpActivity.java
+++ app/src/main/java/com/example/fullfilment_v3/signup/SignUpActivity.java
@@ -3,14 +3,62 @@ package com.example.fullfilment_v3.signup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.fullfilment_v3.R;
+import com.google.android.material.button.MaterialButton;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    public static final String ADD_USER = "addUser";
+    EditText editTextUsername;
+    EditText editTextEmail;
+    EditText editTextFirstName;
+    EditText editTextLastName;
+    EditText editTextPassword;
+    EditText editTextConfirmPassword;
+    MaterialButton btnSignUp;
+    TextView txtViewWarning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        txtViewWarning = findViewById(R.id.txtViewWarning);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String username = editTextUsername.getText().toString().trim();
+                String email = editTextEmail.getText().toString().trim();
+                String firstName = editTextFirstName.getText().toString().trim();
+                String lastName = editTextLastName.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+
+                if(username.isEmpty() || email.isEmpty() || firstName.isEmpty() || lastName.isEmpty() ||
+                        password.isEmpty() || confirmPassword.isEmpty()) {
+                    txtViewWarning.setText("All fields must be completed!");
+                    txtViewWarning.setVisibility(View.VISIBLE);
+                    return;
+                }
+                if(!password.equals(confirmPassword)) {
+                    txtViewWarning.setText("The given passwords do not match");
+                    txtViewWarning.setVisibility(View.VISIBLE);
+                    return;
+                }
+            }
+        });
     }
 }
