958349badfb51bf6d3253a025e31cbb53be581b4
Rares2442 <raresinator66@gmail.com>
2024-04-20 22:03:48 +0300

    Logare cont google

--- app/src/main/java/com/example/fullfilment_v3/signin/SignInActivity.java
+++ app/src/main/java/com/example/fullfilment_v3/signin/SignInActivity.java
@@ -1,23 +1,40 @@
 package com.example.fullfilment_v3.signin;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fullfilment_v3.R;
+import com.example.fullfilment_v3.appactivities.WelcomePageActivity;
 import com.example.fullfilment_v3.asynctask.InsertUserAsyncTask;
 import com.example.fullfilment_v3.asynctask.VerifyUserAsyncTask;
 import com.example.fullfilment_v3.signup.SignUpActivity;
 import com.example.fullfilment_v3.users.User;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -30,6 +47,13 @@ public class SignInActivity extends AppCompatActivity {
     TextView txtForgottenPassword;
     CheckBox showPassword;
 
+    ImageView imgGoogle;
+
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
+    public static final int GOOGLE_LOG_IN = 20;
+    private boolean showOneTapUI = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +65,13 @@ public class SignInActivity extends AppCompatActivity {
         signUpButton = findViewById(R.id.btnSignUp);
         txtForgottenPassword = findViewById(R.id.txtForgotPassword);
         showPassword = findViewById(R.id.cbShowPassword);
+        imgGoogle = findViewById(R.id.imageGoogle);
+
+        oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder().
+                setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder().
+                        setSupported(true).setServerClientId(getString(R.string.web_client_id)).
+                        setFilterByAuthorizedAccounts(false).build()).build();
 
         showPassword.setOnCheckedChangeListener((compoundButton, isChecked) -> {
             if(isChecked) {
@@ -65,6 +96,47 @@ public class SignInActivity extends AppCompatActivity {
         signUpButton.setOnClickListener(view -> {
             intent = new Intent(getApplicationContext(), SignUpActivity.class);
             startActivityForResult(intent, SIGN_UP_REQUEST);
+        });
+
+        ActivityResultLauncher<IntentSenderRequest> activityResultLauncher =
+                registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(),
+                        new ActivityResultCallback<ActivityResult>() {
+                            @Override
+                            public void onActivityResult(ActivityResult result) {
+                                try {
+                                    SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(result.getData());
+                                    String idToken = credential.getGoogleIdToken();
+                                    if(idToken != null) {
+                                        String email = credential.getId();
+                                        String username = credential.getDisplayName();
+                                        Toast.makeText(getApplicationContext(),"Email : "+email+" Name : "+username, Toast.LENGTH_SHORT).show();
+
+                                        Intent intent1 = new Intent(getApplicationContext(), WelcomePageActivity.class);
+                                        intent1.putExtra("user_name", username);
+                                        startActivity(intent1);
+                                    }
+                                } catch (Exception e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                        });
+
+        imgGoogle.setOnClickListener(view -> {
+            oneTapClient.beginSignIn(signInRequest).
+            addOnSuccessListener(SignInActivity.this, new OnSuccessListener<BeginSignInResult>() {
+                @Override
+                public void onSuccess(BeginSignInResult beginSignInResult) {
+
+                    IntentSenderRequest intentSenderRequest =
+                            new IntentSenderRequest.Builder(beginSignInResult.getPendingIntent().getIntentSender()).build();
+                    activityResultLauncher.launch(intentSenderRequest);
+                }
+            }).addOnFailureListener(SignInActivity.this, new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("TAG", e.getLocalizedMessage() );
+                        }
+                    });
         });
     }
 
