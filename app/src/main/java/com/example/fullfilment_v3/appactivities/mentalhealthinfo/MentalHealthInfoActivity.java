444218d66dc5d93397286686db4202ddc613c10b
Rares2442 <raresinator66@gmail.com>
2024-04-29 21:37:26 +0300

    Design pagina sanatate mintala final

--- app/src/main/java/com/example/fullfilment_v3/appactivities/mentalhealthinfo/MentalHealthInfoActivity.java
+++ app/src/main/java/com/example/fullfilment_v3/appactivities/mentalhealthinfo/MentalHealthInfoActivity.java
@@ -1,16 +1,44 @@
 package com.example.fullfilment_v3.appactivities.mentalhealthinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.fullfilment_v3.R;
 
 public class MentalHealthInfoActivity extends AppCompatActivity {
 
+    CardView cardViewPTSD;
+    CardView cardViewDepression;
+    CardView cardViewAnxiety;
+    CardView cardViewADHD;
+    CardView cardViewBipolarDisorder;
+    CardView cardViewOCD;
+    CardView cardViewInsomnia;
+    CardView cardViewSelfEsteem;
+    CardView cardViewSuicidalThoughts;
+    CardView cardViewFocusAndDiscipline;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mental_health_info);
+
+        cardViewADHD = findViewById(R.id.cardViewADHD);
+        cardViewAnxiety = findViewById(R.id.cardViewAnxiety);
+        cardViewBipolarDisorder = findViewById(R.id.cardViewBipolarDisorder);
+        cardViewDepression = findViewById(R.id.cardViewDepression);
+        cardViewFocusAndDiscipline = findViewById(R.id.cardViewFocusAndDiscipline);
+        cardViewInsomnia = findViewById(R.id.cardViewInsomnia);
+        cardViewOCD = findViewById(R.id.cardViewOCD);
+        cardViewSelfEsteem = findViewById(R.id.cardViewSelfEsteem);
+        cardViewSuicidalThoughts = findViewById(R.id.cardViewSuicidalToughts);
+        cardViewPTSD = findViewById(R.id.cardViewPTSD);
+
+        cardViewPTSD.setOnClickListener(view -> {
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/fullfilment_v3/appactivities/mentalhealthinfo/PTSDInfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.fullfilment_v3.appactivities.mentalhealthinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fullfilment_v3.R;
+
+public class PTSDInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ptsdinfo);
+    }
+}
